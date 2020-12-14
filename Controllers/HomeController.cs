2213806b42d5a01ed89312fd8f6671cf15c3b553
Jaime Supo Puma <jaimesupo@gmail.com>
2020-12-13 20:37:10 -0500

    Conexion base de datos  en  Memoria,  Creacion  de archivos iniciales con  Dotnet mvc  new ,  Formulario  de registro, Vista  de registrados

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica3.Data;
+using Practica3.Models;
+
+namespace Practica3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context=context;
+        }
+
+        public IActionResult Index()
+        {
+            var registro = _context.Registrar.ToList();
+
+            return View(registro);
+        }
+          public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ingresar(Registrar objCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(objCliente);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View("Regitrar", objCliente);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
